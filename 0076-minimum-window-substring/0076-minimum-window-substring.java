5721995d4f07f7334dd9dce3604e5233f5525215
saipranav22-commits <dter90011@gmail.com>
2026-02-05 00:51:27 +0530

    Time: 16 ms (61.12%), Space: 47 MB (59.29%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,35 +1,40 @@
 class Solution {
     public String minWindow(String s, String t) {
-      int[]b=new int[128];
-      for(char c:t.toCharArray()){
-        b[c]++;
-      }  
       int st=0;
       int l=0;
-      int min=Integer.MAX_VALUE;;
+      int min=Integer.MAX_VALUE;
       int req=t.length();
+      HashMap<Character,Integer>map=new HashMap<>();
+      for(int i=0;i<t.length();i++){
+        char c=t.charAt(i);
+        map.put(c,map.getOrDefault(c,0)+1);
+      }  
       for(int i=0;i<s.length();i++){
-      char c=s.charAt(i);
-      if(b[c]>0){
-        req--;
-      }
-      b[c]--;
-      while(req==0){
-        if((i-l+1)<min){
+        char c=s.charAt(i);
+        if(map.containsKey(c)){
+            if(map.get(c)>0)
+            req--;
+            map.put(c,map.getOrDefault(c,0)-1);
+        }
+        while(req==0){
+            if((i-l+1)<min){
             min=i-l+1;
             st=l;
+            }
+            char ch=s.charAt(l);
+            if(map.containsKey(ch)){
+                map.put(ch,map.getOrDefault(ch,0)+1);
+            
+            if(map.get(ch)>0){
+                req++;
+            }
+            }
+            l++;
         }
-        char ch=s.charAt(l);
-        b[ch]++;
-        if(b[ch]>0){
-            req++;
-        }
-        l++;
-      }     
       }
-    if(min!=Integer.MAX_VALUE){
+      if(min!=Integer.MAX_VALUE){
         return s.substring(st,st+min);
-    }
-    return "";
+      }
+      return "";
     }
 }
