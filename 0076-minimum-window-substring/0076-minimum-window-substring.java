40a9fbabe5a6d2a48474c794778e3cd20c0ab613
saipranav22-commits <dter90011@gmail.com>
2026-02-06 00:33:10 +0530

    Time: 2 ms (99.6%), Space: 45.6 MB (84.65%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,40 +1,37 @@
 class Solution {
     public String minWindow(String s, String t) {
-      int st=0;
-      int l=0;
-      int min=Integer.MAX_VALUE;
-      int req=t.length();
-      HashMap<Character,Integer>map=new HashMap<>();
-      for(int i=0;i<t.length();i++){
-        char c=t.charAt(i);
-        map.put(c,map.getOrDefault(c,0)+1);
-      }  
-      for(int i=0;i<s.length();i++){
-        char c=s.charAt(i);
-        if(map.containsKey(c)){
-            if(map.get(c)>0)
-            req--;
-            map.put(c,map.getOrDefault(c,0)-1);
+        int st=0;
+        int l=0;
+        int min=Integer.MAX_VALUE;
+        int []fre=new int[128];
+        int req=t.length();
+        for(int i=0;i<t.length();i++){
+           char c=t.charAt(i);
+           fre[c]++;
         }
-        while(req==0){
-            if((i-l+1)<min){
-            min=i-l+1;
-            st=l;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(fre[c]>0){
+                req--;
             }
-            char ch=s.charAt(l);
-            if(map.containsKey(ch)){
-                map.put(ch,map.getOrDefault(ch,0)+1);
-            
-            if(map.get(ch)>0){
-                req++;
+            fre[c]--;
+            while(req==0){
+                if((i-l+1)<min){
+                    min=i-l+1;
+                    st=l;
+                }
+                char ch=s.charAt(l);
+                fre[ch]++;
+                if(fre[ch]>0){
+                    req++;
+                }
+                l++;
             }
-            }
-            l++;
         }
-      }
-      if(min!=Integer.MAX_VALUE){
-        return s.substring(st,st+min);
-      }
-      return "";
+        if(min!=Integer.MAX_VALUE){
+            return s.substring(st,st+min);
+        }else{
+            return "";
+        }
     }
 }
