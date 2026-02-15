6494169f9189abc5781a509df4ff8e8bcb4fc6e9
saipranav22-commits <dter90011@gmail.com>
2026-02-15 23:38:38 +0530

    Time: 9 ms (22.73%), Space: 47.4 MB (34.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,41 +1,39 @@
 class Solution {
-    static  HashMap<Integer,List<Integer>>map;
-    static boolean dfs(int n,boolean[]c){
-       if(c[n]){
-        return false;
-       }
-       if(!map.containsKey(n)||map.get(n).size()==0){
-        return true;
-       }
-       c[n]=true;
-       for(int i:map.get(n)){
-        if(dfs(i,c)==false){
-            return false;
-        }
-       }
-       c[n]=false;
-       map.put(n,new ArrayList<>());
-       return true;
-    }
+    static HashMap<Integer,List<Integer>>map;
+    static boolean check[];
 
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // if(prerequisites.length==0){
-        //     return true;
-        // }
-        map=new HashMap<>();
-        for(int i=0;i<prerequisites.length;i++){
-            int u=prerequisites[i][0];
-            int v=prerequisites[i][1];
-            map.putIfAbsent(u,new ArrayList<>());
-            map.putIfAbsent(v,new ArrayList<>());
-            map.get(u).add(v);
-        }
-        boolean []c=new boolean[numCourses];
-        for(int i=0;i<numCourses;i++){
-        if(!dfs(i,c)){
+    static boolean dfs(int i){
+      if(check[i]){
+        return false;
+      }
+      if(!map.containsKey(i) || map.get(i).size()==0){
+        return true;
+      }
+     check[i]=true;
+      for(int n:map.get(i)){
+        if(dfs(n)==false){
             return false;
         }
+      }
+      check[i]=false;
+      map.put(i,new ArrayList<>());
+      return true;
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+    check=new boolean[numCourses];
+    map=new HashMap<>();
+    for(int i=0;i<prerequisites.length;i++){
+       int u=prerequisites[i][0];
+       int v=prerequisites[i][1];
+       map.putIfAbsent(u,new ArrayList<>());
+       map.putIfAbsent(v,new ArrayList<>());
+       map.get(u).add(v);
+    }
+    for(int i=0;i<numCourses;i++){
+        if(dfs(i)==false){
+            return false;
         }
-        return true;
+    }
+    return true;
     }
 }
