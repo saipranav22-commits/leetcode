5bcc42e02d96ab8e32887b1ccffc5aaa5a391880
saipranav22-commits <dter90011@gmail.com>
2026-03-05 03:42:44 +0530

    Time: 219 ms (59.57%), Space: 43 MB (51.09%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,17 +1,15 @@
 class Solution {
-    public void solveSudoku(char[][] board) {
-        solve(board);
-    }
-
-    private boolean solve(char[][] board) {
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                if (board[row][col] == '.') {
-                    for (char num = '1'; num <= '9'; num++) {
-                        if (isValid(board, row, col, num)) {
-                            board[row][col] = num;
-                            if (solve(board)) return true;
-                            board[row][col] = '.';
+    static boolean solve(char board[][]){
+        for(int row=0;row<9;row++){
+            for(int col=0;col<9;col++){
+                if(board[row][col]=='.'){
+                    for(char num='1';num<='9';num++){
+                        if(valid(board,row,col,num)){
+                            board[row][col]=num;
+                            if(solve(board)){
+                                return true;
+                            }
+                            board[row][col]='.';
                         }
                     }
                     return false;
@@ -20,18 +18,22 @@ class Solution {
         }
         return true;
     }
-
-    private boolean isValid(char[][] board, int row, int col, char num) {
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == num || board[i][col] == num) return false;
+    static boolean valid(char[][]board,int r,int c,char num){
+        for(int i=0;i<9;i++){
+            if(board[r][i]==num|| board[i][c]==num)return false;
         }
-        int startRow = (row / 3) * 3;
-        int startCol = (col / 3) * 3;
-        for (int i = startRow; i < startRow + 3; i++) {
-            for (int j = startCol; j < startCol + 3; j++) {
-                if (board[i][j] == num) return false;
+        int startrow=(r/3)*3;
+        int startcol=(c/3)*3;
+        for(int i=startrow;i<startrow+3;i++){
+            for(int j=startcol;j<startcol+3;j++){
+                if(board[i][j]==num){
+                    return false;
+                }
             }
         }
         return true;
+    }
+    public void solveSudoku(char[][] board) {
+       solve(board); 
     }
 }
