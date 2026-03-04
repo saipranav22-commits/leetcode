47c08dd785a893f395e9c5fec677bf1bcf58bd1d
saipranav22-commits <dter90011@gmail.com>
2026-03-05 02:19:36 +0530

    Time: 4 ms (14.55%), Space: 49.2 MB (8.05%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        return DFS(s, new HashSet<>(wordDict), new HashMap<String, LinkedList<String>>());
+    }       
+
+    List<String> DFS(String s, Set<String> wordDict, HashMap<String, LinkedList<String>> map) {
+        if (map.containsKey(s))
+            return map.get(s);
+
+        LinkedList<String> res = new LinkedList<>();
+
+        if (s.length() == 0) {
+            res.add("");
+            return res;
+        }
+
+        for (String word : wordDict) {
+            if (s.startsWith(word)) {
+                List<String> sublist = DFS(s.substring(word.length()), wordDict, map);
+                for (String sub : sublist)
+                    res.add(word + (sub.isEmpty() ? "" : " ") + sub);
+            }
+        }
+
+        map.put(s, res);
+        return res;
+    }
+}
