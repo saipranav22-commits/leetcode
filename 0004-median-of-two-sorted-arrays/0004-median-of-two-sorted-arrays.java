bf0d1add3e050e2df3c95de9ff5f48d5488c9c43
saipranav22-commits <dter90011@gmail.com>
2026-03-05 21:20:17 +0530

    Time: 1 ms (99.69%), Space: 48.7 MB (87.05%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,41 +3,42 @@ class Solution {
         if (nums1.length > nums2.length) {
             return findMedianSortedArrays(nums2, nums1);
         }
-        int n=nums1.length;
-        int m=nums2.length;
-        int low=0;
-        int high=n;
-        while(low<=high){
-         int part1=(low+high)/2;
-         int part2=(n+m+1)/2-part1;
-         int maxleft1=Integer.MIN_VALUE;
-         int minright1=Integer.MAX_VALUE;
-         int maxleft2=Integer.MIN_VALUE;
-         int minright2=Integer.MAX_VALUE;
-         if(part1!=0){
-            maxleft1=nums1[part1-1];
-         }
-         if(part1!=n){
-            minright1=nums1[part1];
-         }
-         if(part2!=0){
-           maxleft2=nums2[part2-1];
-         }
-         if(part2!=m){
-           minright2=nums2[part2];
-         }
-         if (maxleft1 <= minright2 && maxleft2 <= minright1) {
+
+        int n = nums1.length;
+        int m = nums2.length;
+        int low = 0;
+        int high = n;
+
+        while (low <= high) {
+            int part1 = (low + high) / 2;
+            int part2 = (n + m + 1) / 2 - part1;
+
+            int maxleft1 = Integer.MIN_VALUE;
+            int minright1 = Integer.MAX_VALUE;
+            int maxleft2 = Integer.MIN_VALUE;
+            int minright2 = Integer.MAX_VALUE;
+
+            if (part1 != 0) maxleft1 = nums1[part1 - 1];
+            if (part1 != n) minright1 = nums1[part1];
+
+            if (part2 != 0) maxleft2 = nums2[part2 - 1];
+            if (part2 != m) minright2 = nums2[part2];
+
+            if (maxleft1 <= minright2 && maxleft2 <= minright1) {
                 if ((n + m) % 2 == 0) {
                     return (Math.max(maxleft1, maxleft2) + Math.min(minright1, minright2)) / 2.0;
                 } else {
-                    return (double) Math.max(maxleft1, maxleft2);
+                    return Math.max(maxleft1, maxleft2);
                 }
-            } else if (maxleft1 > minright2) {
+            } 
+            else if (maxleft1 > minright2) {
                 high = part1 - 1;
-            } else {
+            } 
+            else {
                 low = part1 + 1;
             }
         }
-        return 0.0;    
+
+        return 0.0;
     }
 }
