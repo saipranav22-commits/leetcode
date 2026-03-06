9aae950db90f83a5b274a4a830f5175cd66e88ed
saipranav22-commits <dter90011@gmail.com>
2026-03-06 10:33:41 +0530

    Time: 8 ms (84.48%), Space: 46.9 MB (90%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,42 +1,30 @@
 class Solution {
-
-    int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
-    int m, n;
-
+    static int dir[][]={{-1,0},{1,0},{0,-1},{0,1}};
     public int longestIncreasingPath(int[][] matrix) {
-        if(matrix == null || matrix.length == 0) return 0;
-
-        m = matrix.length;
-        n = matrix[0].length;
-
-        int[][] dp = new int[m][n];
-        int maxLen = 0;
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                maxLen = Math.max(maxLen, dfs(matrix, i, j, dp));
+        if(matrix==null || matrix.length==0)return 0;
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int dp[][]=new int[n][m];
+        int maxlen=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                maxlen=Math.max(maxlen,dfs(matrix,i,j,n,m,dp));
             }
         }
-
-        return maxLen;
+        return maxlen;
     }
+    static int dfs(int mat[][],int i,int j,int n,int m,int dp[][]){
+       if(dp[i][j]!=0)return dp[i][j];
+       int max=1;
+       for(int []d:dir){
+        int x=i+d[0];
+        int y=j+d[1];
 
-    private int dfs(int[][] matrix, int i, int j, int[][] dp){
-
-        if(dp[i][j] != 0) return dp[i][j];
-
-        int max = 1;
-
-        for(int[] dir : dirs){
-            int x = i + dir[0];
-            int y = j + dir[1];
-
-            if(x>=0 && x<m && y>=0 && y<n && matrix[x][y] > matrix[i][j]){
-                max = Math.max(max, 1 + dfs(matrix, x, y, dp));
-            }
+        if(x>=0 && y>=0 && x<n && y<m && mat[x][y]>mat[i][j]){
+            max=Math.max(max,1+dfs(mat,x,y,n,m,dp));
         }
-
-        dp[i][j] = max;
-        return max;
+       }
+       dp[i][j]=max;
+       return max;
     }
 }
