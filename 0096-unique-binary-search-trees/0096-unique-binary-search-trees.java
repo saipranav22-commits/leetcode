ee0e13921f0adfec3354199e82f3ebdc7aaae1b5
saipranav22-commits <dter90011@gmail.com>
2026-03-11 00:13:00 +0530

    Time: 0 ms (100%), Space: 42.4 MB (11.8%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,12 +1,18 @@
-public class Solution {
+class Solution {
     public int numTrees(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                dp[i] += dp[j - 1]*dp[i - j];
-            }
+        int[] uniqTree = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            uniqTree[i] = 1;
         }
-        return dp[n];
+
+        for (int nodes = 2; nodes <= n; nodes++) {
+            int total = 0;
+            for (int root = 1; root <= nodes; root++) {
+                total += uniqTree[root - 1] * uniqTree[nodes - root];
+            }
+            uniqTree[nodes] = total;
+        }
+
+        return uniqTree[n];        
     }
 }
