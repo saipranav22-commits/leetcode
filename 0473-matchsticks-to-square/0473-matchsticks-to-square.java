f27fef89219230cbc905ddf9b3dfc2a529ad782d
saipranav22-commits <dter90011@gmail.com>
2026-03-10 21:30:27 +0530

    Time: 107 ms (39.07%), Space: 43.5 MB (33.6%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.java
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.java
@@ -1,41 +1,40 @@
-public class Solution {
-    public boolean makesquare(int[] nums) {
-    	if (nums == null || nums.length < 4) return false;
-        int sum = 0;
-        for (int num : nums) sum += num;
-        if (sum % 4 != 0) return false;
-        
-        Arrays.sort(nums);
-        reverse(nums);
-        
-    	return dfs(nums, new int[4], 0, sum / 4);
-    }
-    
-    private boolean dfs(int[] nums, int[] sums, int index, int target) {
-    	if (index == nums.length) {
-    	    if (sums[0] == target && sums[1] == target && sums[2] == target) {
-    		return true;
-    	    }
-    	    return false;
-    	}
-    	
-    	for (int i = 0; i < 4; i++) {
-    	    if (sums[i] + nums[index] > target) continue;
-    	    sums[i] += nums[index];
-            if (dfs(nums, sums, index + 1, target)) return true;
-    	    sums[i] -= nums[index];
-    	}
-    	
-    	return false;
-    }
-    
-    private void reverse(int[] nums) {
-        int i = 0, j = nums.length - 1;
-        while (i < j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++; j--;
+class Solution {
+    static void reverse(int[]a){
+        int l=0;
+        int h=a.length-1;
+        while(l<h){
+            int temp=a[l];
+            a[l]=a[h];
+            a[h]=temp;
+            l++;
+            h--;
         }
+    }
+    static boolean dfs(int mat[],int sum[],int ind,int target){
+        if(ind==mat.length){
+                if(sum[0]==target && sum[1]==target && sum[2]==target){
+                   return true;
+                }
+            return false;
+        }
+        
+        for(int i=0;i<4;i++){
+            if(sum[i]+mat[ind]>target)
+            continue;
+            sum[i]+=mat[ind];
+           if( dfs(mat,sum,ind+1,target))return true;
+            sum[i]-=mat[ind];
+        }
+        return false;
+    }
+    public boolean makesquare(int[] mat) {
+        int sum=0;
+        for(int i=0;i<mat.length;i++){
+            sum+=mat[i];
+        }
+        if(sum%4!=0)return false;
+        Arrays.sort(mat);
+        reverse(mat);
+        return dfs(mat,new int[4],0,sum/4);
     }
 }
