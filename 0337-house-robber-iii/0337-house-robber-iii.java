61a4b2f6c859b4accdbf8c00b6cad2ecc5db2b11
saipranav22-commits <dter90011@gmail.com>
2026-03-13 14:52:32 +0530

    Time: 0 ms (100%), Space: 46.5 MB (72.11%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -18,14 +18,15 @@ class Solution {
         if(root==null){
             return new int[]{0,0};
         }
-       int left[]=dfs(root.left);
-       int right[]=dfs(root.right);
-       int rob=root.val+left[1]+right[1];
-       int notrob=Math.max(left[0],left[1])+Math.max(right[0],right[1]);
-       return new int[]{rob,notrob};
+        int left[]=dfs(root.left);
+        int right[]=dfs(root.right);
+        int rob=root.val+left[1]+right[1];
+        int notrob=Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+        return new int[]{rob,notrob};
+
     }
     public int rob(TreeNode root) {
-       int dp[]=dfs(root);
-       return Math.max(dp[0],dp[1]); 
+        int dp[]=dfs(root);
+        return Math.max(dp[0],dp[1]);
     }
 }
