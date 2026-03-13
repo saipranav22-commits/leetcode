0a3132cc65503370a3e1b08d7056dee7879bda5e
saipranav22-commits <dter90011@gmail.com>
2026-03-14 01:15:41 +0530

    Time: 6 ms (0.31%), Space: 42.5 MB (13.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-      int dp[]=new int[n+2];
+      int dp[]=new int[n];
       if(n<=2){
         return n;
       }
-      dp[0]=0;
-      dp[1]=1;
-      for(int i=2;i<=n+1;i++){
+      dp[0]=1;
+      dp[1]=2;
+      for(int i=2;i<n;i++){
         dp[i]=dp[i-2]+dp[i-1];
         System.out.println(dp[i]);
       }
-      return dp[n+1]; 
+      return dp[n-1]; 
     }
 }
