ae29e2f2a277413f229a8a5cb54f74761f250e6e
saipranav22-commits <dter90011@gmail.com>
2026-03-17 22:02:05 +0530

    Time: 0 ms (100%), Space: 44.3 MB (91.27%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,22 +1,26 @@
 class Solution {
-    public List<String> generateParenthesis(int n) {
-        char[] cur=new char[2*n];
-        List<String> res=new ArrayList<>();
-        backtracking(0,0,0,cur,res,n);
-        return res;
+    static List<String>res;
+    static char car[];
+    static void bt(int open,int close,int start,int n){
+       if(start==2*n){
+        res.add(new String(car));
+        return;
+       }
+
+       if(open<n){
+        car[start]='(';
+        bt(open+1,close,start+1,n);
+       }
+
+       if(close<open){
+        car[start]=')';
+        bt(open,close+1,start+1,n);
+       }
     }
-    static void backtracking(int start,int open,int close,char[] cur,List<String> res,int n){
-        if(start==2*n){
-            res.add(new String(cur));
-            return;
-        }
-        if (open<n){
-            cur[start]='(';
-            backtracking(start+1,open+1,close,cur,res,n);
-        }
-        if (close<open){
-            cur[start]=')';
-            backtracking(start+1,open,close+1,cur,res,n);
-        }
+    public List<String> generateParenthesis(int n) {
+      car=new char[2*n];
+      res=new ArrayList<>();
+      bt(0,0,0,n);
+      return res;
     }
 }
