037c6af66d73247cef8a1c1cc3c95109f6135849
saipranav22-commits <dter90011@gmail.com>
2026-03-17 16:23:34 +0530

    Time: 299 ms (5.01%), Space: 49.3 MB (76.99%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,50 +1,50 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
-            return 0;
-
-        int M = matrix.length;
-        int N = matrix[0].length;
-
-        int[][] mat = new int[M][N];
-
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                mat[i][j] = matrix[i][j] - '0';
+        int n=matrix.length;
+        int m=matrix[0].length;
+           
+        int[][] mat = new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                mat[i][j]=matrix[i][j]-'0';
             }
         }
 
-        for (int i = 0; i < M; i++) {
-            for (int j = 1; j < N; j++) {
-                if (mat[i][j] == 1) {
-                    mat[i][j] += mat[i][j - 1];
+        for(int i=0;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(mat[i][j]==1)
+                mat[i][j]+=mat[i][j-1];
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(mat[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+
+                int wid=mat[i][j];
+
+                if(wid==0)continue;
+                 int curwid=wid;
+                for(int k=i;k<n && mat[k][j]>0 ;k++){
+                    curwid=Math.min(curwid,mat[k][j]);
+                    int height=k-i+1;
+                    ans=Math.max(ans,curwid*height);
+                }
+
+                curwid=wid;
+                for(int k=i;k>=0 && mat[k][j]>0 ;k--){
+                    curwid=Math.min(curwid,mat[k][j]);
+                    int height=i-k+1;
+                    ans=Math.max(ans,curwid*height);
                 }
             }
         }
-
-        int Ans = 0;
-
-        for (int j = 0; j < N; j++) {
-            for (int i = 0; i < M; i++) {
-                int width = mat[i][j];
-                if (width == 0) continue;
-
-                int currWidth = width;
-                for (int k = i; k < M && mat[k][j] > 0; k++) {
-                    currWidth = Math.min(currWidth, mat[k][j]);
-                    int height = k - i + 1;
-                    Ans = Math.max(Ans, currWidth * height);
-                }
-
-                currWidth = width;
-                for (int k = i; k >= 0 && mat[k][j] > 0; k--) {
-                    currWidth = Math.min(currWidth, mat[k][j]);
-                    int height = i - k + 1;
-                    Ans = Math.max(Ans, currWidth * height);
-                }
-            }
-        }
-
-        return Ans;
+        return ans;
     }
 }
