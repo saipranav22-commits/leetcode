5fe967ae79abbadf75a73a235ab8f8a83e62119f
saipranav22-commits <dter90011@gmail.com>
2026-04-24 00:49:11 +0530

    Time: 0 ms (100%), Space: 43.8 MB (10.47%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,11 +1,11 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int j=0;
+      int j=0;
         for(int i=0;i<nums.length;i++){
             if(val!=nums[i]){
                 nums[j++]=nums[i];
-            }
-        }
-        return j;
+            }  
     }
+    return j;
+}
 }
