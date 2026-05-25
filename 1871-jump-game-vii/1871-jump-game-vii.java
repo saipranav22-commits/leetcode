c1c06a0223614e46bc6bc607a1e5af0470dae6d8
saipranav22-commits <dter90011@gmail.com>
2026-05-25 22:22:01 +0530

    Time: 13 ms (38.37%), Space: 57.7 MB (10.3%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.java
+++ 1871-jump-game-vii/1871-jump-game-vii.java
@@ -1,31 +1,33 @@
 class Solution {
-    public boolean canReach(String str, int minJump, int maxJump) {
-        if(str.charAt(str.length()-1)=='1') return false;
-        List<Integer> list=new ArrayList<>();
-        list.add(0);
-        for(int i=1;i<str.length();i++){
-            if(str.charAt(i)=='0'){
-                int start=i-maxJump,end=i-minJump;
-                int s=0,e=list.size()-1;
-                boolean found=false;
-                while(s<=e){
-                    int m=(s+e)/2;
-                    if(start<=list.get(m) && list.get(m)<=end){
-                        found =true;
-                        break;
-                    }
-                    else if(list.get(m)<start){
-                        s=m+1;
-                    }
-                    else{
-                        e=m-1;
-                    }
-                }
-                if(found){
-                    list.add(i);
+    public boolean canReach(String s, int minJump, int maxJump) {
+        Queue<Integer>q=new LinkedList<>();
+        boolean visited[]=new boolean[s.length()];
+
+        q.add(0);
+        visited[0]=true;
+        int far=0;
+
+        if(s.charAt(s.length()-1)=='1')return false;
+
+        while(!q.isEmpty()){
+            int ind=q.poll();
+            
+            int st=Math.max(ind+minJump,far+1);
+            int end=Math.min(ind+maxJump,s.length()-1);
+
+            for(int i=st;i<=end;i++){
+                if(visited[i])continue;
+
+                if(s.charAt(i)=='0'){
+
+                if(i==s.length()-1)return true;
+                visited[i]=true;
+                q.add(i);
+
                 }
             }
+            far=end;
         }
-        return list.get(list.size()-1)==str.length()-1?true:false;
+        return false;
     }
 }
