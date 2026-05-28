275fed53791bb6a2cb9ed20c4289aa3c6dccb146
saipranav22-commits <dter90011@gmail.com>
2026-05-28 19:11:36 +0530

    Time: 48 ms (84.53%), Space: 284.7 MB (11.6%) - LeetHub

--- 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.java
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.java
@@ -1,50 +1,56 @@
 class Solution {
-    class Node {
-        int bestIdx;
-        Node[] children = new Node[26];
-        Node() {
-            bestIdx = 0;
-        }
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        int bestLen = Integer.MAX_VALUE;
+        int bestIdx = Integer.MAX_VALUE;
     }
-    
+
     public int[] stringIndices(String[] wordsContainer, String[] wordsQuery) {
-        Node root = new Node();
+        TrieNode root = new TrieNode();
         
         for (int i = 0; i < wordsContainer.length; i++) {
-            String w = wordsContainer[i];
-            Node node = root;
+            String word = wordsContainer[i];
+            int len = word.length();
+            TrieNode curr = root;
             
-            if (wordsContainer[node.bestIdx].length() > w.length()) {
-                node.bestIdx = i;
+            if (len < curr.bestLen || (len == curr.bestLen && i < curr.bestIdx)) {
+                curr.bestLen = len;
+                curr.bestIdx = i;
             }
             
-            for (int j = w.length() - 1; j >= 0; j--) {
-                int c = w.charAt(j) - 'a';
-                if (node.children[c] == null) {
-                    node.children[c] = new Node();
-                    node.children[c].bestIdx = i;
-                } else {
-                    if (wordsContainer[node.children[c].bestIdx].length() > w.length()) {
-                        node.children[c].bestIdx = i;
-                    }
+            for (int j = len - 1; j >= 0; j--) {
+                int charIdx = word.charAt(j) - 'a';
+                
+                if (curr.children[charIdx] == null) {
+                    curr.children[charIdx] = new TrieNode();
                 }
-                node = node.children[c];
+                
+                curr = curr.children[charIdx];
+                
+                if (len < curr.bestLen || (len == curr.bestLen && i < curr.bestIdx)) {
+                    curr.bestLen = len;
+                    curr.bestIdx = i;
+                }
             }
         }
         
         int[] ans = new int[wordsQuery.length];
-
-
+        
         for (int i = 0; i < wordsQuery.length; i++) {
-            String q = wordsQuery[i];
-            Node node = root;
-            for (int j = q.length() - 1; j >= 0; j--) {
-                int c = q.charAt(j) - 'a';
-                if (node.children[c] == null) break;
-                node = node.children[c];
+            String query = wordsQuery[i];
+            int len = query.length();
+            TrieNode curr = root;
+            
+            for (int j = len - 1; j >= 0; j--) {
+                int charIdx = query.charAt(j) - 'a';
+                if (curr.children[charIdx] == null) {
+                    break;
+                }
+                curr = curr.children[charIdx];
             }
-            ans[i] = node.bestIdx;
+            ans[i] = curr.bestIdx;
         }
+        
         return ans;
     }
-}
+} 
