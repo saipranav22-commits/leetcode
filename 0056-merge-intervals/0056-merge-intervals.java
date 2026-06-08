727bb13280adab969af81a30839e4d7e5641c6e2
saipranav22-commits <dter90011@gmail.com>
2026-06-08 23:16:32 +0530

    Time: 122 ms (5.86%), Space: 49.4 MB (5.68%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -2,6 +2,7 @@ class Solution {
     public int[][] merge(int[][] intervals) {
 
         Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        
         for(int i=0;i<intervals.length;i++){
             for(int j=0;j<intervals[0].length;j++){
                 System.out.print(intervals[i][j]+" ");
@@ -9,7 +10,9 @@ class Solution {
             System.out.println();
         }
         int pre[]=intervals[0];
+
         List<int[]>res=new ArrayList<>();
+
         for(int i=1;i<intervals.length;i++){
             int current[]=intervals[i];
 
