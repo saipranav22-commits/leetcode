3e4ea7b8667710233a8ce0ac481ca6a9c7a9b85b
saipranav22-commits <dter90011@gmail.com>
2026-06-10 10:15:05 +0530

    Time: 7 ms (21.78%), Space: 47.3 MB (12.25%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,32 +1,44 @@
 class Solution {
     public String convert(String s, int numRows) {
-       if (numRows == 1 || numRows >= s.length()) {
+        if (numRows == 1 || numRows >= s.length()) {
             return s;
         }
 
         int idx = 0, d = 1;
-        List<Character>[] rows = new ArrayList[numRows];
+        List<List<Character>> l = new ArrayList<>();
         for (int i = 0; i < numRows; i++) {
-            rows[i] = new ArrayList<>();
+            l.add(new ArrayList<>());
         }
+        int ind = 0;
+        int pos = 0;
 
         for (char c : s.toCharArray()) {
-            rows[idx].add(c);
-            if (idx == 0) {
-                d = 1;
-            } else if (idx == numRows - 1) {
-                d = -1;
+            l.get(pos).add(c);
+
+            if (ind == 0) {
+                if (pos == numRows - 1) {
+                    ind = 1;
+                    pos--;
+                } else {
+                    pos++;
+                }
+            } else {
+                if (pos == 0) {
+                    ind = 0;
+                    pos++;
+                } else {
+                    pos--;
+                }
             }
-            idx += d;
         }
 
         StringBuilder result = new StringBuilder();
-        for (List<Character> row : rows) {
+        for (List<Character> row : l) {
             for (char c : row) {
                 result.append(c);
             }
         }
 
-        return result.toString();        
+        return result.toString();
     }
 }
