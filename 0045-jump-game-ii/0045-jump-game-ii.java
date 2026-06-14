bff70ba892463a14e4c8c8f79f9dc05fb9d188d8
saipranav22-commits <dter90011@gmail.com>
2026-06-14 17:45:04 +0530

    Time: 1 ms (99.72%), Space: 47.3 MB (37.83%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,17 +1,26 @@
 class Solution {
     public int jump(int[] nums) {
-       int max=0;
-       int n=nums.length;
-       int count=0;
-       int end=0;
-       for(int i=0;i<nums.length-1;i++){
-        max=Math.max(max,i+nums[i]);
-        if(i==end){
+      int max=0;
+      int n=nums.length;
+
+      int count=0;
+
+      int end=0;
+
+      for(int i=0;i<n-1;i++){
+       max=Math.max(max,nums[i]+i);
+
+       if(i==end){
         count++;
         end=max;
-        }
-        if(end>=n-1)return count;
-       } 
-       return count;
+       }
+
+       if(end>=n-1){
+        return count;
+       }
+
+      } 
+
+      return count; 
     }
 }
