cb7ca8a2f3f9fd369456a356ad6dc09fb9a5c94a
saipranav22-commits <dter90011@gmail.com>
2026-06-14 17:12:21 +0530

    Time: 2 ms (88.38%), Space: 48 MB (41.81%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,8 +3,11 @@ class Solution {
         int n=nums.length;
         int max=0;
     for(int i=0;i<n;i++){
+
         if(i>max)return false;
+
         max=Math.max(max,i+nums[i]);
+        
     }
     return true;
     }
