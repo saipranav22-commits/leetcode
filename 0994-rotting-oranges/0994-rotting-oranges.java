6c2f032da43cb744561e51e39c9db2c6d27acb7b
saipranav22-commits <dter90011@gmail.com>
2026-06-25 13:59:39 +0530

    Time: 2 ms (90.7%), Space: 43.9 MB (83.91%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,61 +1,58 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
 
-        int n = grid[0].length;
+        int n=grid.length;
+        int m=grid[0].length;
 
-        int[][] visited = grid;
+        int min=-1;
+        int orange=0;
 
-        Queue<int[]> q = new LinkedList<>();
+        int [][]dir={{1,0},{-1,0},{0,1},{0,-1}};
 
-        int countFreshOrange = 0;
+        int vis[][]=grid;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visited[i][j] == 2) {
-                    q.offer(new int[] {i, j});
+        Queue<int[]>q=new LinkedList<>();
+
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==2){
+                    q.add(new int[]{i,j});
                 }
-                if (visited[i][j] == 1) {
-                    countFreshOrange++;
+                if(vis[i][j]==1){
+                    orange++;
                 }
             }
         }
-          
-             
-        if (countFreshOrange == 0)
-            return 0;
-
-        if (q.isEmpty())
-            return -1;
-
-        
-        int minutes = -1;
-
-        int[][] dirs = {{1, 0},{-1, 0},{0, -1},{0, 1}};
 
 
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int k=0;k<size;k++) {
-                int[] cell = q.poll();
-                int x = cell[0];
-                int y = cell[1];
-                for (int[] dir : dirs) {
-                    int i = x + dir[0];
-                    int j = y + dir[1];
-                    if (i >= 0 && i < m && j >= 0 && j < n && visited[i][j] == 1) {
-                        visited[i][j] = 2;
-                        countFreshOrange--;
-                        q.offer(new int[] {i, j});
-                    }
-                }
+        if(orange==0)return 0;
+
+        if(q.isEmpty())return -1;
+
+
+        while(!q.isEmpty()){
+            int size=q.size();
+
+            for(int k=0;k<size;k++){
+               int cell[]=q.poll();
+               int x=cell[0];
+               int y=cell[1];
+
+               for(int []d:dir){
+                 int i=x+d[0];
+                 int j=y+d[1];
+
+                 if(i>=0 && i< n && j>=0 && j<m && vis[i][j]==1){
+                    vis[i][j]=2;
+                    q.add(new int[]{i,j});
+                    orange--;
+                 }
+               }
             }
-            minutes++;
+            min++;
         }
-
-        
-        if (countFreshOrange == 0)
-            return minutes;
+        if(orange==0)return min;
 
         return -1;
     }
