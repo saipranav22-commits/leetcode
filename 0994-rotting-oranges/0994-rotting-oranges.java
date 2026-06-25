4b206e99f230427a4265f00a93aee0bcd5450bfc
saipranav22-commits <dter90011@gmail.com>
2026-06-25 10:33:26 +0530

    Time: 2 ms (90.7%), Space: 44.2 MB (55.97%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,56 +1,62 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int c=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1){
-                    grid[i][j]=Integer.MAX_VALUE;
-                }
-                else if(grid[i][j]==2){
-                    grid[i][j]=0;
-                }
-                else{
-                  grid[i][j]=-1;
-                }
-            }
-            
-        }
-        
-        int n=grid.length;
-        int m=grid[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==0){
-                    func(grid,i,j,0);
-                    
-                }
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==Integer.MAX_VALUE){
-                    return -1;
-                }
-                c=Math.max(c,grid[i][j]);
-            }
-        }
-        return c;
-        
-    }
-    public static void func(int[][] grid ,int i,int j,int time){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==-1){
-            return;
-        } 
-        if(grid[i][j]<time){
-            return;
-        }
-        grid[i][j]=time;
-        
-        
-        func(grid,i+1,j,time+1);
-        func(grid,i,j+1,time+1);
-        func(grid,i-1,j,time+1);
-        func(grid,i,j-1,time+1);
+        int m = grid.length;
 
+        int n = grid[0].length;
+
+        int[][] visited = grid;
+
+        Queue<int[]> q = new LinkedList<>();
+
+        int countFreshOrange = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (visited[i][j] == 2) {
+                    q.offer(new int[] {i, j});
+                }
+                if (visited[i][j] == 1) {
+                    countFreshOrange++;
+                }
+            }
+        }
+          
+             
+        if (countFreshOrange == 0)
+            return 0;
+
+        if (q.isEmpty())
+            return -1;
+
+        
+        int minutes = -1;
+
+        int[][] dirs = {{1, 0},{-1, 0},{0, -1},{0, 1}};
+
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int k=0;k<size;k++) {
+                int[] cell = q.poll();
+                int x = cell[0];
+                int y = cell[1];
+                for (int[] dir : dirs) {
+                    int i = x + dir[0];
+                    int j = y + dir[1];
+                    if (i >= 0 && i < m && j >= 0 && j < n && visited[i][j] == 1) {
+                        visited[i][j] = 2;
+                        countFreshOrange--;
+                        q.offer(new int[] {i, j});
+                    }
+                }
+            }
+            minutes++;
+        }
+
+        
+        if (countFreshOrange == 0)
+            return minutes;
+
+        return -1;
     }
 }
