51a5d66cce4d4162d5677d9a99303144ada1ed07
saipranav22-commits <dter90011@gmail.com>
2026-08-09 19:58:51 +0530

    Time: 8 ms (61.51%), Space: 43.2 MB (53.38%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -1,26 +1,47 @@
-  class Solution{
-    public boolean canPartitionKSubsets(int[] A, int k) {
-        if (k > A.length) return false;
-        int sum = 0;
-        for (int num : A) sum += num;
-        if (sum % k != 0) return false;
-        boolean[] visited = new boolean[A.length];
-        Arrays.sort(A);
-        return dfs(A, 0, A.length - 1, visited, sum / k, k);
-    }
-
-    public boolean dfs(int[] A, int sum, int st, boolean[] visited, int target, int round) {
-        if (round == 0) return true;
-        if (sum == target && dfs(A, 0, A.length - 1, visited, target, round - 1))
-            return true;
-        for (int i = st; i >= 0; --i) {
-            if (!visited[i] && sum + A[i] <= target) {
-                visited[i] = true;
-                if (dfs(A, sum + A[i], i - 1, visited, target, round))
-                    return true;
-                visited[i] = false;
-            }
+class Solution {
+    static boolean vis[];
+    static int count;
+    public static boolean back(int nums[],int k,int d,int n,int sum,int st){
+       
+       if(count==k-1)return true;
+       if(sum==d){
+        count++;
+        if(back(nums,k,d,n,0,0)){
+        return true;
         }
+        count--;
         return false;
+       }
+
+    for(int i=st;i<n;i++){
+    if(sum+nums[i]>d)continue;
+    if(vis[i])continue;
+
+
+        if(i>0 && nums[i]==nums[i-1] && !vis[i-1])continue;
+
+        vis[i]=true;
+
+        if(back(nums,k,d,n,sum+nums[i],i+1))return true;
+
+        vis[i]=false;
     }
+    
+    return false;
     }
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum=0;
+        count=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        int n=nums.length;
+        vis=new boolean[n];
+
+        if(sum%k!=0)return false;
+        Arrays.sort(nums);
+        int d=sum/k;
+
+        return back(nums,k,d,n,0,0);
+    }
+}
